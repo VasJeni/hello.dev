551e9cd7094cde9314e1c80439090f8a1dfeac0e
JasJeni <Vasilyev508@gmail.com>
2018-04-12 12:01:13 +0300

    new push

--- index.php
+++ index.php
@@ -5,9 +5,8 @@
     <title>Title</title>
 </head>
 <body>
-<a href="loginform/loginform.php">enter</a>
+<a href="loginform/loginform.php">Enter form</a>
 <br />
 <a href="calculate/calculate.php"> Calculate</a>
-
 </body>
 </html>
