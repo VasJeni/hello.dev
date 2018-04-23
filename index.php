819ec71bed020ead313f2f161c6afe5c06d5fb3f
JasJeni <Vasilyev508@gmail.com>
2018-04-23 18:02:06 +0300

    app victorines

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 $question = " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet atque commodi consectetur eos esse ex explicabo illo impedit in neque nihil odit optio perferendis provident, qui quidem reprehenderit. Perferendis, rerum.";
 require 'View/pageWithQuestion.php';
+$question=1;
